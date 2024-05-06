fd4f05be72d59327ee7f091eb96bffa8ef5120d9
Namrata <124352728+Namsrkive@users.noreply.github.com>
2024-05-06 21:53:39 +0530

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Restaurant Rating Prediction")
+st.subheader("The main goal of this is to get insights on restaurants which people like visit and to identify the rating of the restaurant.")
+st.write("Restaurant Rating has become the most commonly used parameter for judging a restaurant for any individual.\
+          Rating of a restaurant depends on factors like reviews, area situated, average cost for two people, votes, \
+          cuisines and the type of restaurant.")
